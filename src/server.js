d96091d693513ddbb1f66a5e36a6e62293c8ed8c
wahyunugrahha <wahyunih297@gmail.com>
2025-07-14 17:58:10 +0700

    Refactoring Error Handling in Handler.js

--- src/server.js
+++ src/server.js
@@ -2,6 +2,8 @@ const Hapi = require('@hapi/hapi');
 const notes = require('./api/notes');
 const NotesService = require('./service/inMemory/NotesService');
 const NotesValidator = require('./validator/notes');
+const ClientError = require('./api/exceptions/ClientError');
+
 const init = async () => {
   const notesService = new NotesService();
   const server = Hapi.server({
@@ -20,6 +22,23 @@ const init = async () => {
       service: notesService,
       validator: NotesValidator,
     },
+  });
+
+  server.ext('onPreResponse', (request, h) => {
+    // mendapatkan konteks response dari request
+    const { response } = request;
+
+    // penanganan client error secara internal.
+    if (response instanceof ClientError) {
+      const newResponse = h.response({
+        status: 'fail',
+        message: response.message,
+      });
+      newResponse.code(response.statusCode);
+      return newResponse;
+    }
+
+    return h.continue;
   });
 
   await server.start();
