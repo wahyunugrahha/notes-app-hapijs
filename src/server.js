29cc6b9daa9966905ced0b8505214f3016d584a9
wahyunugrahha <wahyunih297@gmail.com>
2025-07-15 22:05:06 +0700

    Changing Port

--- src/server.js
+++ src/server.js
@@ -7,7 +7,7 @@ const ClientError = require('./api/exceptions/ClientError');
 const init = async () => {
   const notesService = new NotesService();
   const server = Hapi.server({
-    port: 3000,
+    port: 5000,
     host: process.env.NODE_ENV !== 'production' ? 'localhost' : '0.0.0.0',
     routes: {
       cors: {
