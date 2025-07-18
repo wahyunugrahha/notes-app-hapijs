f098181463afe8abbdf04c2bccb5393e2e1f6794
wahyunugrahha <wahyunih297@gmail.com>
2025-07-18 21:53:47 +0700

    Add Database

--- src/server.js
+++ src/server.js
@@ -1,14 +1,16 @@
+require('dotenv').config();
+
 const Hapi = require('@hapi/hapi');
 const notes = require('./api/notes');
-const NotesService = require('./service/inMemory/NotesService');
+const NotesService = require('./service/postgres/NotesService');
 const NotesValidator = require('./validator/notes');
 const ClientError = require('./api/exceptions/ClientError');
 
 const init = async () => {
   const notesService = new NotesService();
   const server = Hapi.server({
-    port: 5000,
-    host: process.env.NODE_ENV !== 'production' ? 'localhost' : '0.0.0.0',
+    port: process.env.PORT,
+    host: process.env.HOST,
     routes: {
       cors: {
         origin: ['*'],
