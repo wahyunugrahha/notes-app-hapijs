d95a5359c650c4979b7e1fd5bc29069e32fc40e2
wahyunugrahha <wahyunih297@gmail.com>
2025-07-30 11:53:09 +0700

    Fix Bug

--- src/api/users/handler.js
+++ src/api/users/handler.js
@@ -5,7 +5,7 @@ class UsersHandler {
     this._validator = validator;
 
     this.postUserHandler = this.postUserHandler.bind(this);
-    this.getUserByIdHandler = this.getUserByIdHandler.bind(this); 
+    this.getUserByIdHandler = this.getUserByIdHandler.bind(this);
   }
 
   async postUserHandler(request, h) {
